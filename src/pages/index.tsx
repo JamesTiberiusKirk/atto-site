526b48de6f1991178f326c6adfe48ff68571dcbd
JamesTiberiusKirk <dumitru.v.dv@gmail.com>
2023-04-12 14:34:38 +0100

    Some cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
-import Link from "next/link";
-import AttoPage from "~/components/page";
+import Link from 'next/link';
+import AttoPage from '~/components/page';
 import { GoKebabVertical } from 'react-icons/go'
 import { useEffect, useState } from 'react';
 
@@ -63,7 +63,7 @@ export default function Home() {
                             <Image
                                 src='/workshop/dvelped_nice_pic_scaled.png'
                                 alt='Picture of workshop'
-                                // style={{ objectFit: "cover" }}
+                                // style={{ objectFit: 'cover' }}
                                 style={{ objectFit: 'cover' }}
                                 priority
                                 fill
@@ -71,20 +71,20 @@ export default function Home() {
                         </div>
                     </div>
 
-                    <div className="w-full gap-4 grid over-xl:grid-flow-col bellow-xl:grid-flow-row over-xl:px-20 over-xl:pl-40 ">
-                        <div className="bellow-sm:p-16 px-16 py-10 text-[#8C2F00] text-3xl">
+                    <div className='w-full gap-4 grid over-xl:grid-flow-col bellow-xl:grid-flow-row over-xl:px-20 over-xl:pl-40 '>
+                        <div className='bellow-sm:p-16 px-16 py-10 text-[#8C2F00] text-3xl'>
                             <div>
                                 <h1>
                                     Acting workshops with
                                     the UK&apos;s leading directors
                                 </h1>
-                                <div className="h-5" />
+                                <div className='h-5' />
                                 <Link
-                                    className="px-5 py-3 bg-[#8C2F00] text-gray-100 font-bold rounded-full"
+                                    className='px-5 py-3 bg-[#8C2F00] text-gray-100 font-bold rounded-full'
                                     href={'/apply'}>Apply</Link>
                             </div>
                             <div className='h-5' />
-                            <div className="bellow-xl:pt-5 bellow-xl:w-full bellow-xl:object-center " >
+                            <div className='bellow-xl:pt-5 bellow-xl:w-full bellow-xl:object-center ' >
                                 <div className='over-xl:absolute bottom-16 left-16 over-xl:left-52 '>
                                     atto supports
                                     <div
@@ -104,10 +104,10 @@ export default function Home() {
                             </div>
                         </div>
 
-                        <div className="place-content-center w-full over-xl:py-16">
+                        <div className='place-content-center w-full over-xl:py-16'>
                             <Image
                                 className='object-center mx-auto'
-                                alt="Atto logo"
+                                alt='Atto logo'
                                 width={1890 / 3}
                                 height={1417 / 3}
                                 src='/logo_with_name.png'
@@ -117,32 +117,34 @@ export default function Home() {
                 </div>
             </div >
             <div id='workshops'>
-                <div className='h-screen w-full grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 bg-white'>
-                    <div className="p-10 container flex flex-col  text-[#8C2F00] text-3xl">
-                        <div className="h-full ltr ">
+                <div className='min-h-screen w-full grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 bg-white'>
+                    <div className='p-10 container flex flex-col  text-[#8C2F00] text-3xl'>
+                        <div className='h-full ltr '>
                             <h1>
                                 Workshops
                             </h1>
-                            <div className="h-10" />
+                            <div className='h-10' />
                         </div>
                     </div>
                 </div>
             </div >
             <div id='what-we-do'>
-                <div className='h-screen w-full grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 bg-[#FF955F]'>
-                    <div className="p-10 container flex flex-col text-[#8C2F00] text-3xl">
-                        <div className="h-full ltr ">
-                            <h1>
+                <div className='min-h-screen w-full grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 bg-[#FF955F]'>
+                    <div className='p-10 container flex flex-col text-[#8C2F00]'>
+                        <div className='h-full ltr '>
+                            <h1 className='text-3xl'>
                                 What we do
                             </h1>
-                            <p>
-                                What we do
-
+                            <p className='py-2'>
                                 Professional actors with professional directors
-
+                            </p>
+                            <p className='py-2'>
                                 On Saturdays professional actors of all ages are welcome to take part in our drop-in scene studies. Our aim is to create a truly safe space to fail, succeed, learn & work on that craft with the guidance of leading directors.
+                            </p>
+                            <p className='py-2'>
                                 Workshops will take place at Downside Fisher Youth Club, Coxson Place, Druid Street, London, SE1 2EZ. A 10 minute walk from London Bridge Station.
-
+                            </p>
+                            <p className='py-2'>
                                 Workshops help support the running of the Downside Fisher Youth Club who help socially excluded children & young people from Bermondsey & its neighbouring areas.
                             </p>
                         </div>
@@ -150,38 +152,46 @@ export default function Home() {
                 </div>
             </div >
             <div id='testimonials'>
-                <div className='h-screen w-full grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 bg-white'>
-                    <div className="p-10 container flex flex-col  text-[#8C2F00] text-3xl">
-                        <div className="h-full ltr ">
-                            <h1>
+                <div className='min-h-screen w-full grid grid-cols-1  bg-white'>
+                    <div className='p-10 text-[#8C2F00] '>
+                        <div className='h-full ltr '>
+                            <h1 className='text-3xl text-center'>
                                 Testimonials
                             </h1>
-                            <p>
-                                Ayo - “I really loved the workshop because I learned something new that was very effective I will take with me for the rest of my acting career”
+                            <div className='max-w-screen-sm'>
+                                <p className='py-2'>
+                                    Ayo - “I really loved the workshop because I learned something new that was very effective I will take with me for the rest of my acting career”
+                                </p>
 
-                                Ella Dorman-Gajic - “ An engaging and informative 2 hours, learning from a highly skilled director”
+                                <p className='py-2'>
+                                    Ella Dorman-Gajic - “ An engaging and informative 2 hours, learning from a highly skilled director”
+                                </p>
 
-                                Atto Participant - “Great! Fun location and very inclusive”
+                                <p className='py-2'>
+                                    Atto Participant - “Great! Fun location and very inclusive”
+                                </p>
 
-                                Atto Participant - “ I liked that I didn’t feel pressure or nervous in the room, the director was friendly and communicative, easy to talk to and discuss where to go with the text”
+                                <p className='py-2'>
+                                    Atto Participant - “ I liked that I didn’t feel pressure or nervous in the room, the director was friendly and communicative, easy to talk to and discuss where to go with the text”
+                                </p>
 
-                                Atto Participant - “I learnt about the importance of punctuation!”
-                            </p>
+                                <p className='py-2'>
+                                    Atto Participant - “I learnt about the importance of punctuation!”
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div >
-            <div id='news-letter'>
-                <div className='h-screen w-full grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 bg-white'>
-                    <div className="p-10 container flex flex-col  text-[#8C2F00] text-3xl">
-                        <div className="h-full ltr ">
-                            <h1>
-                                Subscribe to our news letter
-                            </h1>
-                        </div>
+            <div id='news-letter' className='h-96 w-full bg-[#FF955F]'>
+                <div className='p-10 text-[#8C2F00] '>
+                    <div className='h-full ltr'>
+                        <h1 className='text-3xl text-center'>
+                            Subscribe to our news letter
+                        </h1>
                     </div>
                 </div>
-            </div >
+            </div>
         </AttoPage >
     )
 }
