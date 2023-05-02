fce1bc18bda2f20d0cba49dc084725a2f2b9e9ed
JamesTiberiusKirk <dumitru.v.dv@gmail.com>
2023-05-02 10:01:27 +0100

    Added protection on the cron

--- src/pages/api/cron/admin_reporting.ts
+++ src/pages/api/cron/admin_reporting.ts
@@ -2,8 +2,10 @@ import { getAllApplicationsInPast } from 'lib/db/application'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function AdminReportingHandler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.query['key'] !== 'hardcodedKey2231') return res.status(404).end()
+
     const applications = await getAllApplicationsInPast(24)
-    if (!applications) res.status(500)
+    if (!applications) return res.status(500).end()
 
     res.status(200).json(applications)
 }
