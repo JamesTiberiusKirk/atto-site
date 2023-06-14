3f5ed5cab3d8d8cf4f61e04505772da1b8b3f704
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2023-06-14 22:23:59 +0100

    Quick fiexes (#2)

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -8,6 +8,8 @@ import { getWorkshopByKey } from "types/workshop";
 import AttoPage from "~/components/page";
 import type { ContactUs } from "~/server/api/routers/contactus";
 import { BsCloudDownload } from "react-icons/bs";
+import { CSVLink } from "react-csv";
+import Link from "next/link";
 
 type AdminProps = {
   user: LoginRequest;
@@ -17,16 +19,22 @@ type AdminProps = {
 };
 
 export default function Admin(props: AdminProps) {
+  // console.log(props);
+  // const date = new Date();
+  // const dateString = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
   return (
     <AttoPage>
-      <div className="flex min-h-screen flex-col items-center bg-[#FF955F] px-4 py-16">
+      <div className="flex min-h-screen flex-col items-center whitespace-pre-line bg-[#FF955F] px-4 py-16">
         <div className="inline-flex">
           <h1 className="text-3xl text-white ">Admin: {props.user.email}</h1>
         </div>
         <div className="max-w-2/3 inline rounded-lg bg-white p-5">
           <div className="pt-3 pl-3">
             <h1 className="text-2xl">Applications</h1>
-            <BsCloudDownload className="flex items-end" />
+            {/* <Link href="/api/csv/applications"> */}
+            {/*   <BsCloudDownload className="flex items-end" /> */}
+            {/*   Download CSV */}
+            {/* </Link> */}
           </div>
           <table>
             <thead>
@@ -40,25 +48,33 @@ export default function Admin(props: AdminProps) {
               </tr>
             </thead>
             <tbody>
-              {props.applications.map((a, k) => (
-                <tr key={k}>
-                  <td className="border px-4 py-2">{a.name}</td>
-                  <td className="border px-4 py-2">{a.email}</td>
-                  <td className="border px-4 py-2">{a.credits}</td>
-                  <td className="border px-4 py-2">{a.pronouns}</td>
-                  <td className="border px-4 py-2">
-                    {a.workshops.map((w, k) => (
-                      <span key={k}>
-                        {getWorkshopByKey(w)?.instructorName}
-                        {k < a.workshops.length && <br />}
-                      </span>
-                    ))}
-                  </td>
-                  <td className="border px-4 py-2">
-                    {!a.emailPreference ? "opt in" : "opt out"}
+              {props.applications ? (
+                props.applications.map((a, k) => (
+                  <tr key={k}>
+                    <td className="border px-4 py-2">{a.name}</td>
+                    <td className="border px-4 py-2">{a.email}</td>
+                    <td className="border px-4 py-2">{a.credits}</td>
+                    <td className="border px-4 py-2">{a.pronouns}</td>
+                    <td className="border px-4 py-2">
+                      {a.workshops.map((w, k) => (
+                        <span key={k}>
+                          {getWorkshopByKey(w)?.instructorName}
+                          {k < a.workshops.length && <br />}
+                        </span>
+                      ))}
+                    </td>
+                    <td className="border px-4 py-2">
+                      {a.emailPreference ? "opt in" : "opt out"}
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={6} className="border px-4 py-2 text-center">
+                    No Applications
                   </td>
                 </tr>
-              ))}
+              )}
             </tbody>
           </table>
 
@@ -66,6 +82,13 @@ export default function Admin(props: AdminProps) {
 
           <div className="pt-3 pl-3">
             <h1 className="text-2xl">Contact Requests</h1>
+            {/* <CSVLink */}
+            {/*   data={props.contactUsRequests} */}
+            {/*   filename={`contact-us-requests-${dateString}.csv`} */}
+            {/* > */}
+            {/*   <BsCloudDownload className="flex items-end" /> */}
+            {/*   Download CSV */}
+            {/* </CSVLink> */}
           </div>
           <table>
             <thead>
@@ -77,14 +100,22 @@ export default function Admin(props: AdminProps) {
               </tr>
             </thead>
             <tbody>
-              {props.contactUsRequests.map((c, k) => (
-                <tr key={k}>
-                  <td className="border px-4 py-2">{c.name}</td>
-                  <td className="border px-4 py-2">{c.email}</td>
-                  <td className="border px-4 py-2">{c.pronouns}</td>
-                  <td className="border px-4 py-2">{c.message}</td>
+              {props.contactUsRequests ? (
+                props.contactUsRequests.map((c, k) => (
+                  <tr key={k}>
+                    <td className="border px-4 py-2">{c.name}</td>
+                    <td className="border px-4 py-2">{c.email}</td>
+                    <td className="border px-4 py-2">{c.pronouns}</td>
+                    <td className="border px-4 py-2">{c.message}</td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={4} className="border px-4 py-2 text-center">
+                    No Contact Requests
+                  </td>
                 </tr>
-              ))}
+              )}
             </tbody>
           </table>
 
@@ -100,11 +131,17 @@ export default function Admin(props: AdminProps) {
               </tr>
             </thead>
             <tbody>
-              {props.applications.map((a, k) => (
-                <tr key={k}>
-                  <td className="border px-4 py-2">{a.email}</td>
+              {props.subscriptions ? (
+                props.subscriptions.map((a, k) => (
+                  <tr key={k}>
+                    <td className="border px-4 py-2">{a}</td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td className="border px-4 py-2">No Subscriptions</td>
                 </tr>
-              ))}
+              )}
             </tbody>
           </table>
         </div>
