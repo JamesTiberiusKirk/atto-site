342e9f30ccaf5131ec30ff7f804226a3e0385b2f
JamesTiberiusKirk <dumitru.v.dv@gmail.com>
2023-12-03 20:49:56 +0000

    December update

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,29 +1,30 @@
-import Head from "next/head"
-
-
+import Head from "next/head";
 
 type AttoPageProps = {
-    children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 export default function AttoPage({ children }: AttoPageProps) {
-    const ogImageUrl = 'https://attoworkshops.com/api/og'
-    return (
-        <>
-            <Head>
-                <title>atto </title>
-                <meta name="description" content="atto" />
-                <link rel="icon" href="/favicon.ico" />
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:site" content="@attoworkshops" />
-                <meta name="twitter:title" content="atto workshops" />
-                <meta name="twitter:description" content="Checkout and apply to atto workshops." />
-                <meta name="twitter:image" content={ogImageUrl} />
-                <meta name="og:image" content={ogImageUrl} />
-            </Head>
-            <main>
-                {children}
-            </main >
-        </>
-    )
+  const ogImageUrl = "https://attoworkshops.com/api/og";
+  return (
+    <>
+      <Head>
+        <title>atto </title>
+        <meta name="description" content="atto" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@attoworkshops" />
+        <meta name="twitter:title" content="atto workshops" />
+        <meta
+          name="twitter:description"
+          content="Checkout and apply to atto workshops."
+        />
+        <meta name="twitter:image" content={ogImageUrl} />
+        <meta name="og:image" content={ogImageUrl} />
+      </Head>
+      <main>
+        <div className="w-full min-w-fit">{children}</div>
+      </main>
+    </>
+  );
 }
